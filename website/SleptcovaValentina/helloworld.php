964f532715a535890d7766d379320edc9fbf9155
Mikhail <mikhail.antonoff@yandex.ru>
2018-12-04 20:50:41 +0900

    Changed something

--- website/SleptcovaValentina/helloworld.php
+++ website/SleptcovaValentina/helloworld.php
@@ -3,7 +3,7 @@
   <title>Testing our very PHP</title>
  </head>
  <body>
- <?php echo '<p>Hello, world from Valentina!</p>'; ?>
+ <?php echo '<p>Hello, world from Valentina ver2!</p>'; ?>
  <?php phpinfo(); ?>
  </body>
 </html>

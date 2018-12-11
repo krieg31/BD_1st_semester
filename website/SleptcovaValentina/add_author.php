6bf7734e00ee814bc46fb220c3997da40f9e2726
NEFUstudents <study.svfu@gmail.com>
2018-12-11 18:12:27 +0900

    comething new

--- website/SleptcovaValentina/add_author.php
+++ website/SleptcovaValentina/add_author.php
@@ -4,7 +4,8 @@ $link = mysqli_connect('10.14.129.132', 'SlepcovaValentina', 'Dc6LZquV','Slepcov
 
 $first_name = $_POST['first_name'];
 $family_name = $_POST['family_name'];
-
+echo $first_name;
+echo $family_name;
 //$SQLquery = "INSERT INTO authors (AuthorID, FirstName, FamilyName) VALUES ((SELECT max(AuthorID) from authors) + 1, '$first_name','$family_name')";
 //if(!mysql_query($link,$SQLquery)) {
 //        echo $SQLquery;

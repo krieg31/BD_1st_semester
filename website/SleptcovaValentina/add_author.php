9c18345c67d4b51ab6bde9ec12f69cc3a4c6d334
NEFUstudents <study.svfu@gmail.com>
2018-12-11 18:08:02 +0900

    comething new

--- website/SleptcovaValentina/add_author.php
+++ website/SleptcovaValentina/add_author.php
@@ -1,12 +1,3 @@
-	$link = 
-	    
-	printf('<P>Succesfully connected!</P> %s',"\n");
-	
-	// Выполняем SQL-запрос
-	$SQLquery = 'SELECT * FROM authors INNER JOIN books on books.AuthorID=authors.AuthorID';
-	$SQLresult = mysqli_query($link,$SQLquery);
-
-
 <?php
 $link = mysqli_connect('10.14.129.132', 'SlepcovaValentina', 'Dc6LZquV','SlepcovaValentinaDB')
 	or die('Error: Unable to connect: ' . mysqli_connect_error());
@@ -16,9 +7,11 @@ $family_name = $_POST['family_name'];
 
 $SQLquery = "INSERT INTO authors (AuthorID, FirstName, FamilyName) VALUES ((SELECT max(AuthorID) from authors) + 1, '$first_name','$family_name')";
 if(!mysql_query($link,$SQLquery)) {
-    echo 'Data not inserted';
+        echo $SQLquery;
+	echo 'Data not inserted';
 } else {
-    echo 'Data inserted';
+	echo $SQLquery;
+    	echo 'Data inserted';
 }
 //header("refresh:2; url=form.html");
 ?>

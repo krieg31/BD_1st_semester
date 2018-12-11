7990a2fc375d09b40189542839ce94b6282c45f6
NEFUstudents <study.svfu@gmail.com>
2018-12-11 17:46:25 +0900

    comething new

--- website/SleptcovaValentina/deploy.bat
+++ website/SleptcovaValentina/deploy.bat
@@ -9,22 +9,4 @@ git commit -a -m "comething new"
 echo Объединяем все изменения из удаленного репозитория с локальной версией
 git pull origin master
 echo Применение коммита, мерж локальных измнений в удаленный репозиторий
-=======
-
-echo Глобальная настйрока переменных, достаточно выполнить 1 раз
-
-git config --global user.name NEFUmasters
-git config --global user.email study.svfu@gmail.com
-
-echo Добавим трекинг всех новых файлов
-git add -A
-
-git commit -a -m "comething new"
-
-echo Объединяем все изменения из удаленного репозитория с локальной версией
-
-git pull origin master
-
-echo Применение коммита, мерж локальных измнений в удаленный репозиторий
->>>>>>> e30bcc3102c6616e6672107619dd40ae2bae515e
 git push origin master

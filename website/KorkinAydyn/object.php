b7c284d1ba92dedeb484c9b18e5d9bf8977b95e3
NEFUstudent <study.svfu@gmail.com>
2018-12-13 16:30:24 +0900

    comething new

--- website/KorkinAydyn/object.php
+++ website/KorkinAydyn/object.php
@@ -1,7 +1,14 @@
+<html>
+ <head>
+  <title>WEB-site of the KorkinAydyn</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ </head>
+ <body>
 <?php
-printf('<P>Hello World! Address </P>');
-$link = mysqli_connect('10.14.129.132','MigalkinEgor','urtvddBq','MigalkinEgorDB') or die('Error! You suck!'.mysqli_connect_error());
+printf('<P>Hello World! object  </P>');
+$link = mysqli_connect(($server, $user, $password, $database) or die('Error! You suck!'.mysqli_connect_error());
 echo '<P>Successfully connected<\P>';
+
 $SQLquery = 'SELECT * FROM Object';
 $SQLresult = mysqli_query($link,$SQLquery);
 while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
@@ -11,7 +18,11 @@ while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
 		printf('</TR> %s',"\n");
 	}
 	printf('</table> %s',"\n");
-	
+
 mysqli_free_result($SQLresult);
 mysqli_close($link);
 ?>
+<BR>
+<a href="index.html"> <P>GO BACK</P> </a>
+ </body>
+</html>

931d758cb9da2b004ede41c83192f1e102a7a63a
NEFUstudent <study.svfu@gmail.com>
2018-12-13 16:16:03 +0900

    comething new

--- website/AAR/allobjects.php
+++ website/AAR/allobjects.php
@@ -11,7 +11,7 @@
 	    or die('Error: Unable to connect: ' . mysqli_connect_error());
 	printf('<P>Succesfully connected!</P> %s',"\n");
 	
-	$SQLquery = 'SELECT * FROM customers';
+	$SQLquery = 'SELECT * FROM objects';
 	$SQLresult = mysqli_query($link,$SQLquery);
 
 	printf('<table cellspacing=\' 0 \' border=\' 1 \'> %s',"\n");

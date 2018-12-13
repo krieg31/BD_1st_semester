136459534738b8a62e0f18c8014f26809703d8a9
NEFUmasters <study.svfu@gmail.com>
2018-12-13 16:19:40 +0900

    comething new

---
+++ website/AleksNikitaSavAnton/foto.php
@@ -0,0 +1,36 @@
+<html>
+ <head>
+  <title>WEB-site of the Alekseev Nikta's and Savvin Anton's website rieltors</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ </head>
+ <body>
+	<?php
+	printf('<P>Hello world! Searching for every human:</P> %s',"\n");
+
+	$link = mysqli_connect('10.14.129.132', 'AlekseevNikita', '9YFJ9skR','AlekseevNikitaDB')
+	    or die('Error: Unable to connect: ' . mysqli_connect_error());
+	printf('<P>Succesfully connected!</P> %s',"\n");
+
+	$SQLquery = 'SELECT * FROM 4elovek';
+	$SQLresult = mysqli_query($link,$SQLquery);
+
+	printf('<table cellspacing=\' 0 \' border=\' 1 \'> %s',"\n");
+	printf('<TR> %s',"\n");
+	printf('	<TH>foto</TH> %s',"\n");
+	printf('	<TH>kvartiri_id_kvartiri</TH> %s',"\n");
+	printf('</TR> %s',"\n");
+
+
+	while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
+	{
+		printf('<TR>');
+		printf('<TD> %s </TD> <TD> %s </TD>',$result[1],$result[2]);
+		printf('</TR> %s',"\n");
+	}
+	printf('</table> %s',"\n");
+	mysqli_free_result($SQLresult);
+	mysqli_close($link);
+
+	?>
+ </body>
+</html>

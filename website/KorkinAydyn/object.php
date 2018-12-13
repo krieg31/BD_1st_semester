20511d7b066d4e2a265857a83bb04cec1f5c03dc
NEFUstudent <study.svfu@gmail.com>
2018-12-13 16:09:53 +0900

    comething new

--- website/KorkinAydyn/object.php
+++ website/KorkinAydyn/object.php
@@ -4,6 +4,14 @@ $link = mysqli_connect('10.14.129.132','MigalkinEgor','urtvddBq','MigalkinEgorDB
 echo '<P>Successfully connected<\P>';
 $SQLquery = 'SELECT * FROM Object';
 $SQLresult = mysqli_query($link,$SQLquery);
+while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
+	{
+		printf('<TR>');
+		printf('<TD> %s </TD> <TD>%s</TD> <TD> %s (%d) </TD>',$result[1],$result[2],$result[5],$result[6]);
+		printf('</TR> %s',"\n");
+	}
+	printf('</table> %s',"\n");
+	
 mysqli_free_result($SQLresult);
 mysqli_close($link);
 ?>

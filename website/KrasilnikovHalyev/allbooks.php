9b8f3b45922eba3a917c0b4eb1e78c673cc92f69
NEFUstudent <study.svfu@gmail.com>
2018-12-13 16:54:44 +0900

    comething new

--- website/KrasilnikovHalyev/allbooks.php
+++ website/KrasilnikovHalyev/allbooks.php
@@ -6,32 +6,29 @@
  <body>
 	<?php
 	printf('<P>Hello world! Searching for every book:</P> %s',"\n");
-	// Соединяемся, выбираем базу данных VER3
+
 	
 	$link = mysqli_connect('10.14.129.132', 'KrasilnikovEgor','qmBsLnFN','KrasilnikovEgorDB')
 	    or die('Error: Unable to connect: ' . mysqli_connect_error());
 	printf('<P>Succesfully connected!</P> %s',"\n");
 	
-	// Выполняем SQL-запрос
 	$SQLquery = 'SELECT * FROM patient';
 	$SQLresult = mysqli_query($link,$SQLquery);
 
 	printf('<table cellspacing=\' 0 \' border=\' 1 \'> %s',"\n");
 	printf('<TR> %s',"\n");
-	printf('	<TH>First Name</TH> %s',"\n");
-	printf('	<TH>Family Name</TH> %s',"\n");
-	printf('	<TH>Book</TH> %s',"\n");
+	printf('	<TH>Name</TH> %s',"\n");
+	printf('	<TH>family_name</TH> %s,'"\n")
 	printf('</TR> %s',"\n");
 
 
 	while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
 	{
 		printf('<TR>');
-		printf('<TD> %s </TD> <TD>%s</TD> <TD> %s (%d) </TD>',$result[1],$result[2],$result[5],$result[6]);
+		printf('<TD> %s </TD> <TD> %s </TD> ',$result[2],$result[3]);
 		printf('</TR> %s',"\n");
 	}
 	printf('</table> %s',"\n");
-	// Освобождаем память от результата
 	mysqli_free_result($SQLresult);
 	mysqli_close($link);
 

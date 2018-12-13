b291673d8ed5051460d1a278a9101a68dc2e489d
NEFUstudent <study.svfu@gmail.com>
2018-12-13 16:43:52 +0900

    comething new

--- website/KorkinAydyn/object.php
+++ website/KorkinAydyn/object.php
@@ -6,8 +6,23 @@
  <body>
 <?php
 printf('<P>Hello World! object  </P>');
+
+include('config.php');	
 $link = mysqli_connect(($server, $user, $password, $database) or die('Error! You suck!'.mysqli_connect_error());
 echo '<P>Successfully connected<\P>';
+
+$SQLquery = 'SELECT * FROM Object';
+$SQLresult = mysqli_query($link,$SQLquery);
+while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
+	{
+		printf('<TR>');
+		printf('<TD> %s </TD> <TD>%s</TD> <TD> %s (%d) </TD>',$result[1],$result[2],$result[5],$result[6]);
+		printf('</TR> %s',"\n");
+	}
+	printf('</table> %s',"\n");
+
+mysqli_free_result($SQLresult);
+mysqli_close($link);
 
 ?>
 <BR>

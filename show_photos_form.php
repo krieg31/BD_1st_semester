cb26f4b81b902c9d73dec3098aac106392f96231
krieg31 <krieg31@users.noreply.github.com>
2018-12-29 00:44:24 +0900

    Update show_photos_form.php

--- show_photos_form.php
+++ show_photos_form.php
@@ -6,21 +6,18 @@ $objects_idobjects = mysqli_real_escape_string($con, $_POST['objects_idobjects']
 
 $SQLquery = "SELECT photoscol FROM photos WHERE objects_idobjects = $objects_idobjects";
 $SQLresult = mysqli_query($con,$SQLquery);
-$varA = 0;
+$result = mysqli_fetch_array($SQLresult,MYSQLI_NUM);
 printf('<table width="100%" cellspacing="0" border="1">',"\n");	
 printf('<TR>',"\n");		
 printf('<TH>photos</TH>',"\n");	
 printf('</TR>',"\n");		
-	
-	while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
-	{
-		printf('<TR>',"\n");
-		printf('<TD> <img src=%s alt=""> </TD>',$result[$varA]);
-		printf("<br>");
-		$varA=$varA+1;
-	}
+for($i = 0, $size = count($result); $i < $size; ++$i) {
+	printf('<TR>',"\n");
+	printf('<TD> <img src=%s alt=""> </TD>',$result[$i]);
+	printf("<br>");
+}
 printf('</table>',"\n");
-
 mysqli_close($con);
 printf('<a href="index.html"> <P>GO BACK</P> </a>');
+
 ?>

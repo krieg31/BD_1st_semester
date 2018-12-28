0c0f933d5407d6f55c3c2f2f219d7a21c5a02938
krieg31 <krieg31@users.noreply.github.com>
2018-12-29 01:39:35 +0900

    Update show_photos_form.php

--- show_photos_form.php
+++ show_photos_form.php
@@ -5,6 +5,7 @@ $idobject = mysqli_real_escape_string($con, $_POST['objects_idobjects']);
 
 $SQLquery = "SELECT photoscol FROM photos WHERE objects_idobjects = $idobject";
 $SQLresult = mysqli_query($con,$SQLquery);
+echo $SQLresult;
 $result = mysqli_fetch_array($SQLresult,MYSQLI_BOTH);
 printf('<table width="100%" cellspacing="0" border="1">',"\n");	
 printf('<TR>',"\n");		

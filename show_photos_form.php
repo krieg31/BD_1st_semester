9c0650aa834d294e460f0b7ca58e8f7e0c956be3
krieg31 <krieg31@users.noreply.github.com>
2018-12-29 01:17:05 +0900

    Update show_photos_form.php

--- show_photos_form.php
+++ show_photos_form.php
@@ -13,7 +13,7 @@ printf('<TH>photos</TH>',"\n");
 printf('</TR>',"\n");		
 for($i = 0, $size = count($result); $i <= $size; ++$i) {
 	printf('<TR>',"\n");
-	printf('<TD> <img src=%s alt=""> </TD>',$result[$i]);
+	printf('<TD> <img src='+ echo $result[$i] +'%s alt=""> </TD>',);
 }
 printf('</table>',"\n");
 mysqli_close($con);

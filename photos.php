0a39e60ae68dab8070f63d50c65c72bed9286a92
krieg31 <krieg31@users.noreply.github.com>
2018-12-28 23:42:26 +0900

    Update photos.php

--- photos.php
+++ photos.php
@@ -18,43 +18,39 @@
         <a href="allobjects.php"> <P>Объекты</P> </a>
         <a href="photos.php"> <P>Фотографии</P> </a>
         <a href="add_object.php"> <P>Добавить новый объект</P> </a>
+        <a href="add_customer.php"> <P>Добавить нового клиента</P> </a>
         <a href="index.html"> <P>Главная страница</P> </a>
 		</div>
 		<div class="content">
-			<?php
-	printf('<P>Hello world! Searching for every customer:</P> %s',"\n");
-	
-	require_once('connect.php');
-	
-	$SQLquery = 'SELECT * FROM photos';
-	$SQLresult = mysqli_query($con,$SQLquery);
-	$varA;
-
-	printf('<table cellspacing=\' 0 \' border=\' 1 \'> %s',"\n");
-	printf('<TR> %s',"\n");
-	printf('	<TH>photos</TH> %s',"\n");
-	printf('</TR> %s',"\n");
-
-
-	while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
-	{
-		printf('<TR>');
-		printf('<TD> %s </TD>',$result[2]);
-		$varA = $result[2];
-		printf('</TR> %s',"\n");
-	}
-	printf('</table> %s',"\n");
-	mysqli_free_result($SQLresult);
-	mysqli_close($con);
-
-?>
+			
 <table width="100%" cellspacing="0" border="1">	
 		<TR>
 			<TH>Table column 1</TH>
 		</TR>
 		<TR>
 			<TD>
-			  <img src=<?php echo $varA;?> alt="This is how my library will look like" />			
+
+				<form action="add_object_form.php" method="post">
+
+					<select name="idowner">
+						<?php 
+						require_once('connect.php');
+	
+						$SQLquery = 'SELECT objects_idobjects FROM photos';
+						$SQLresult = mysqli_query($con,$SQLquery);
+						while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
+						{
+							printf('<option value=%d> %s </option>',$result[0],$result[0]);
+						}
+
+
+						mysqli_free_result($SQLresult);
+						mysqli_close($con);
+						?>
+					</select>
+          		  	<br>
+            		  	<input type="submit" value="show photos">
+      			  </form>
 			</TD>
 		</TR>
 </table>

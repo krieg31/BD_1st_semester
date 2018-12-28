a936bfe399215d01f7aab7a80ec935a0a3cd5db5
krieg31 <krieg31@users.noreply.github.com>
2018-12-29 01:00:56 +0900

    Update show_photos_form.php

--- show_photos_form.php
+++ show_photos_form.php
@@ -4,7 +4,7 @@ require_once('connect.php');
 $objects_idobjects = mysqli_real_escape_string($con, $_POST['objects_idobjects']);
 
 
-$SQLquery = "SELECT photoscol FROM photos WHERE objects_idobjects = $objects_idobjects";
+$SQLquery = "SELECT photoscol FROM photos WHERE objects_idobjects == $objects_idobjects";
 $SQLresult = mysqli_query($con,$SQLquery);
 $result = mysqli_fetch_array($SQLresult,MYSQLI_NUM);
 echo count($result);
